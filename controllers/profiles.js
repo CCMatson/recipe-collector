ec16f0ed8188fc1cbc8c2a8431e3a84059347c11
CCMatson <catherine.matson@gmail.com>
2023-01-14 20:44:37 -0800

    add ability to view one users profiles

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -16,7 +16,24 @@ import { Profile } from '../models/profile.js'
     })
   }
 
+  function show(req, res) {
+    Profile.findById(req.params.id)
+    .then(profile => {
+      const isSelf = profile._id.equals(req.user.profile._id)
+      res.render("profiles/show", {
+        title: `${profile.name}'s profile`,
+        profile,
+        isSelf,
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.redirect("/profiles")
+    })
+  }
+
 
 export {
-  index
+  index,
+  show
 }

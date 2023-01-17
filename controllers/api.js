5d9ada8a5b52a5ba0ef3136db863d21551129f4c
CCMatson <catherine.matson@gmail.com>
2023-01-17 12:21:31 -0800

    start to style the new form

--- controllers/api.js
+++ controllers/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 function ingredients(req, res) {
-  axios.get(`https://api.edamam.com/api/food-database/v2/parser?q=${searchQuery}&app_id=${process.env.EDAMAM.API.ID}&app_key=${process.env.EDAMAM_API_KEY}`)
+  axios.get(`https://api.edamam.com/api/food-database/v2/parser?q=${searchQuery}&app_id=${process.env.EDAMAM_API_ID}&app_key=${process.env.EDAMAM_API_KEY}`)
   .then(response => {
     console.log(response.data , 'response data')
     res.render('recipes' , {
